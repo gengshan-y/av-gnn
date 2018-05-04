df7a203c5ea32efdad1edee8e6360f3abddff0b9
gengshan-y <luanyingjian@gmail.com>
2018-05-04 00:44:30 -0400

    more routes

--- data_collection.py
+++ data_collection.py
@@ -5,6 +5,8 @@ from google.protobuf.json_format import MessageToJson
 import os
 import math
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 from matplotlib import pyplot as plt
 import sys
 sys.path.insert(0,'/home/gengshay/code/carla-agent//PythonClient')
@@ -42,6 +44,14 @@ def poses_town01():
     Each matrix is a new task. We have all the four tasks
 
     """
+    def _poses_navigation():
+            return [[19, 66], [79, 14], [19, 57], [23, 1],
+                    [53, 76], [42, 13], [31, 71], [33, 5],
+                    [54, 30], [10, 61], [66, 3], [27, 12],
+                    [79, 19], [2, 29], [16, 14], [5, 57],
+                    [70, 73], [46, 67], [57, 50], [61, 49], [21, 12],
+                    [51, 81], [77, 68], [56, 65], [43, 54]]
+
 
     def _poses_straight():
        return [[36, 40], [39, 35], [110, 114], [7, 3], [0, 4],
@@ -57,30 +67,29 @@ def poses_town01():
                 [78, 44], [68, 85], [41, 102], [95, 70], [68, 129],
                 [84, 69], [47, 79], [110, 15], [130, 17], [0, 17]]
 
-    return [_poses_straight(),
-            _poses_one_curve()]
+    return [_poses_navigation()]
 
 # town01
 weathers = [1, 3, 6, 8, 4, 14]
 poses_tasks = poses_town01()
-vehicles_tasks = [20, 20]
-pedestrians_tasks = [50, 50]
+vehicles_tasks = [20]
+pedestrians_tasks = [50]
 
 cam0 = carla.sensor.Camera('im0', PostProcessing='SceneFinal')
 cam0.set_image_size(1152, 384)
 cam0.set_position(x=0., y=-6, z=165)
 
-cam1 = carla.sensor.Camera('im1', PostProcessing='SceneFinal')
-cam1.set_image_size(1152, 384)
-cam1.set_position(x=0., y=48, z=165)
+#cam1 = carla.sensor.Camera('im1', PostProcessing='SceneFinal')
+#cam1.set_image_size(1152, 384)
+#cam1.set_position(x=0., y=48, z=165)
 
 depth_cam0 = carla.sensor.Camera('depth0', PostProcessing='Depth')
 depth_cam0.set_image_size(1152, 384)
 depth_cam0.set_position(x=0., y=-6, z=165)
 
-depth_cam1 = carla.sensor.Camera('depth1', PostProcessing='Depth')
-depth_cam1.set_image_size(1152, 384)
-depth_cam1.set_position(x=0., y=48, z=165)
+#depth_cam1 = carla.sensor.Camera('depth1', PostProcessing='Depth')
+#depth_cam1.set_image_size(1152, 384)
+#depth_cam1.set_position(x=0., y=48, z=165)
 
 seg_cam = carla.sensor.Camera('seg', PostProcessing='SemanticSegmentation')
 seg_cam.set_image_size(1152, 384)
@@ -100,15 +109,17 @@ for weather in weathers:
             NumberOfVehicles=vehicles,
             NumberOfPedestrians=pedestrians,
             WeatherId=weather,
-            SeedVehicles=123456789,
-            SeedPedestrians=123456789
+            SeedVehicles=weather*iteration,
+            SeedPedestrians=weather*iteration
+            #SeedVehicles=123456789,
+            #SeedPedestrians=123456789
         )
         
         # Add all the cameras that were set for this experiments
         conditions.add_sensor(cam0)
-        conditions.add_sensor(cam1)
+        #conditions.add_sensor(cam1)
         conditions.add_sensor(depth_cam0)
-        conditions.add_sensor(depth_cam1)
+        #conditions.add_sensor(depth_cam1)
         conditions.add_sensor(seg_cam)
         #conditions.add_sensor(lidar)
 
@@ -131,29 +142,40 @@ with make_carla_client('localhost', 2000) as client:
 
         for pose in experiment.poses:
             for rep in range(experiment.repetitions):
-                seq_name ='ped-%d-veh-%d-wea-%d-pos-%d-%d-rep%d'%\
-                             (experiment.conditions.NumberOfPedestrians,
-                              experiment.conditions.NumberOfVehicles,
+                seq_name ='w%d_pos%d-%d_rep%d'%\
+                             (
                               experiment.conditions.WeatherId,
                               pose[0],pose[1],rep)
                 mkdir_p('./data/%s'%seq_name)
                 mkdir_p('./data/%s/cam0'%seq_name)
-                mkdir_p('./data/%s/cam1'%seq_name)
                 mkdir_p('./data/%s/depth0'%seq_name)
-                mkdir_p('./data/%s/depth1'%seq_name)
                 mkdir_p('./data/%s/seg'%seq_name)
                 mkdir_p('./data/%s/measures'%seq_name)
                 mkdir_p('./data/%s/waypoints'%seq_name)
+                mkdir_p('./data/%s/maps'%seq_name)
 
                 start_point = pose[0]
                 end_point = pose[1]
                 target = positions[end_point]
                 print('%d to %d'%(start_point,end_point))
 
+                # draw for data
+                ax.clear()
+                ax.imshow(town_map)
+                pixel = carla_map.convert_to_pixel([positions[start_point].location.x,
+                                    positions[start_point].location.y,
+                                    positions[start_point].location.z])
+                ax.add_patch(Circle((pixel[0], pixel[1]), 12, color='r', label='A point'))
+                pixel = carla_map.convert_to_pixel([positions[end_point].location.x,
+                                                    positions[end_point].location.y,
+                                                    positions[end_point].location.z])
+                ax.add_patch(Circle((pixel[0], pixel[1]), 12, color='b', label='A point'))
+
 
                 client.start_episode(start_point)
                
-                for i in range(50):
+                # warm up
+                for i in range(30):
                     measurements, sensor_data = client.read_data()
                     client.send_control(measurements.player_measurements.autopilot_control)
 
@@ -177,51 +199,59 @@ with make_carla_client('localhost', 2000) as client:
                         (target.location.x, target.location.y, 22),
                         (target.orientation.x, target.orientation.y, -0.001))
 
-                    if i%1==0:
-                        ax.clear()
-                        ax.imshow(town_map)
-                        pixel = carla_map.convert_to_pixel([positions[end_point].location.x,
-                                                            positions[end_point].location.y,
-                                                            positions[end_point].location.z])
-                        ax.add_patch(Circle((pixel[0], pixel[1]), 2, color=(0,0,0), label='destination'))
+                
+                    # draw for data
+                    if i%10==0:
                         pixel = carla_map.convert_to_pixel([curr_x,curr_y,22])
-                        ax.add_patch(Circle((pixel[0], pixel[1]), 2, color='r', label='current'))
-                        # waypoints
-                        for loc in wp_info['waypoints'][::20]:
-                            pixel = carla_map.convert_to_pixel([loc[0],loc[1],22])
-                            ax.add_patch(Circle((pixel[0], pixel[1]), 2, color='g'))
-                        for ag in measurements.non_player_agents:
-                            if ag.HasField('vehicle'):
-                                loc = [ag.vehicle.transform.location.x, ag.vehicle.transform.location.y]
-                                if sldist([curr_x, curr_y],[loc[0], loc[1]]) < 10000:
-                                    pixel = carla_map.convert_to_pixel([loc[0],loc[1],22])
-                                    ax.add_patch(Circle((pixel[0], pixel[1]), 2, color=(1,0,1), label='vehicle'))
-                            if ag.HasField('pedestrian'):
-                                loc = [ag.pedestrian.transform.location.x, ag.pedestrian.transform.location.y]
-                                if sldist([curr_x, curr_y],[loc[0], loc[1]]) < 10000:
-                                    pixel = carla_map.convert_to_pixel([loc[0],loc[1],22])
-                                    ax.add_patch(Circle((pixel[0], pixel[1]), 2, color='b', label='pedestrain'))
+                        ax.add_patch(Circle((pixel[0], pixel[1]), 12, color='g', label='A point'))
                         fig.canvas.draw()
-                        plt.savefig('data/%s/map-%05d.png'%(seq_name,i),dpi=200)
+
+                 #   # draw function for demo
+                 #   if i%1==0:
+                 #       ax.clear()
+                 #       ax.imshow(town_map)
+                 #       pixel = carla_map.convert_to_pixel([positions[end_point].location.x,
+                 #                                           positions[end_point].location.y,
+                 #                                           positions[end_point].location.z])
+                 #       ax.add_patch(Circle((pixel[0], pixel[1]), 10, color=(0,0,0), label='destination'))
+                 #       pixel = carla_map.convert_to_pixel([curr_x,curr_y,22])
+                 #       ax.add_patch(Circle((pixel[0], pixel[1]), 10, color='r', label='current'))
+                 #       # waypoints
+                 #       for loc in wp_info['waypoints'][::20]:
+                 #           pixel = carla_map.convert_to_pixel([loc[0],loc[1],22])
+                 #           ax.add_patch(Circle((pixel[0], pixel[1]), 2, color='g'))
+                 #       for ag in measurements.non_player_agents:
+                 #           if ag.HasField('vehicle'):
+                 #               loc = [ag.vehicle.transform.location.x, ag.vehicle.transform.location.y]
+                 #               if sldist([curr_x, curr_y],[loc[0], loc[1]]) < 8000:
+                 #                   pixel = carla_map.convert_to_pixel([loc[0],loc[1],22])
+                 #                   ax.add_patch(Circle((pixel[0], pixel[1]), 5, color=(1,0,1), label='vehicle'))
+                 #           if ag.HasField('pedestrian'):
+                 #               loc = [ag.pedestrian.transform.location.x, ag.pedestrian.transform.location.y]
+                 #               if sldist([curr_x, curr_y],[loc[0], loc[1]]) < 5000:
+                 #                   pixel = carla_map.convert_to_pixel([loc[0],loc[1],22])
+                 #                   ax.add_patch(Circle((pixel[0], pixel[1]), 5, color='b', label='pedestrain'))
+                 #       ax.axis('off')
+                 #       fig.canvas.draw()
+                 #       fig.tight_layout(pad=0)
+                 #       plt.savefig('data/%s/maps/%05d.png'%(seq_name,i),dpi=200)
 
                     client.send_control(control)
-               #     # record
-               #     im0=Image.fromarray(sensor_data['im0'].data, mode='RGB')
-               #     im1=Image.fromarray(sensor_data['im1'].data, mode='RGB')
-               #     depth0 = (sensor_data['depth0'].data*256*256).astype(np.uint16)
-               #     depth1 = (sensor_data['depth1'].data*256*256).astype(np.uint16)
-               #     seg = Image.fromarray(sensor_data['seg'].data)
+                    # record
+                    im0=Image.fromarray(sensor_data['im0'].data, mode='RGB')
+                    depth0 = (sensor_data['depth0'].data*256*256).astype(np.uint16)
+                    seg = Image.fromarray(sensor_data['seg'].data)
 
-               #     im0.save('data/%s/cam0/%05d.png'%(seq_name,i))
-               #     im1.save('data/%s/cam1/%05d.png'%(seq_name,i))
-               #     cv2.imwrite('data/%s/depth0/%05d.png'%(seq_name,i),depth0)
-               #     cv2.imwrite('data/%s/depth1/%05d.png'%(seq_name,i),depth1)
-               #     seg.save('data/%s/seg/%05d.png'%(seq_name,i))
+                    im0.save('data/%s/cam0/%05d.png'%(seq_name,i))
+                    cv2.imwrite('data/%s/depth0/%05d.png'%(seq_name,i),depth0)
+                    seg.save('data/%s/seg/%05d.png'%(seq_name,i))
 
-               #     with open("data/%s/waypoints/%05d.json"%(seq_name,i), 'w') as jsfile:
-               #         json.dump(wp_info,jsfile)
-               #     with open("data/%s/measures/%05d.json"%(seq_name,i), 'w') as jsfile:
-               #         actual_json_text = MessageToJson(measurements)
-               #         actual_json_text = actual_json_text[:-1] + ',"command": %d}'%int(direction)
-               #         jsfile.write( actual_json_text )
-               #     
+                    with open("data/%s/waypoints/%05d.json"%(seq_name,i), 'w') as jsfile:
+                        json.dump(wp_info,jsfile)
+                    with open("data/%s/measures/%05d.json"%(seq_name,i), 'w') as jsfile:
+                        actual_json_text = MessageToJson(measurements)
+                        actual_json_text = actual_json_text[:-1] + ',"command": %d}'%int(direction)
+                        jsfile.write( actual_json_text )
+                # draw for data
+                plt.savefig('data/%s-map.png'%seq_name)
+                    
--- map_vis.py
+++ map_vis.py
@@ -1,13 +1,47 @@
 from moviepy.editor import ImageSequenceClip
 from matplotlib import pyplot as plt
 import numpy as np
+import pdb
 
 ld = []
 for i in range(38):
-    im = plt.imread('/home/gengshay/code/av-gnn/data/ped-50-veh-20-wea-1-pos-7-3-rep0/map-%05d.png'%i)*255
-    im = im.astype(np.uint8)
-    print(np.max(im))
-    ld.append(im)
+    cam0 = plt.imread('/data/gengshay/carla-data/ped-50-veh-20-wea-1-pos-7-3-rep0/cam0/%05d.png'%i)
+    depth0 = plt.imread('/data/gengshay/carla-data/ped-50-veh-20-wea-1-pos-7-3-rep0/depth0/%05d.png'%i)
+    seg = plt.imread('/data/gengshay/carla-data/ped-50-veh-20-wea-1-pos-7-3-rep0/seg/%05d.png'%i)
+    fig = plt.figure(figsize=(8,8))
+
+    ax = fig.add_subplot('311')
+    ax.axis('off')
+    plt.imshow(cam0)
+
+    ax = fig.add_subplot('312')
+    ax.axis('off')
+    plt.imshow(np.log(depth0),cmap='gnuplot2')
+
+    ax = fig.add_subplot('313')
+    ax.axis('off')
+    plt.imshow(seg,cmap='tab20b')
+
+    fig.tight_layout(pad=0)
+    fig.canvas.draw()
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    
+
+    fig, (a0, a1) = plt.subplots(1,2, gridspec_kw = {'width_ratios':[1, 1.5]}, figsize=(24,10))
+    a0.axis('off')
+    a0.imshow(data)
+
+    mapi = plt.imread('/home/gengshay/code/av-gnn/data/ped-50-veh-20-wea-1-pos-7-3-rep0/map-%05d.png'%i)
+    a1.axis('off')
+    a1.imshow(mapi)
+
+    fig.tight_layout(pad=0)
+    fig.canvas.draw()
+
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    ld.append(data)
 
 clip = ImageSequenceClip(ld,fps=5)
-clip.write_videofile('map_vis.mp4',fps=5)
+clip.write_videofile('vis.mp4',fps=5)
